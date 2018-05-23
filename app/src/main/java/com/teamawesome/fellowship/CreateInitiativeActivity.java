2ba8bd731f658fdf7e661dc4933c3eed40b46f48
Len <laronson21@gmail.com>
2018-05-22 21:26:59 -0400

    First pass of create initiative page

--- app/src/main/java/com/teamawesome/fellowship/CreateInitiativeActivity.java
+++ app/src/main/java/com/teamawesome/fellowship/CreateInitiativeActivity.java
@@ -1,13 +1,25 @@
 package com.teamawesome.fellowship;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CreateInitiativeActivity extends AppCompatActivity {
+
+    @BindView(R.id.input_initiativeTitle) EditText _initiativeTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_initiative);
+        ButterKnife.bind(this);
+
+        _initiativeTitle.setOnFocusChangeListener(new TextFocusChangeListener(getString(R.string.title_instruction), _initiativeTitle));
     }
+
+
 }
