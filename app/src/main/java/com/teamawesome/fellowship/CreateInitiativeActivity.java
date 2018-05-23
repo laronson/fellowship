8067061341fdfc7850547d25f6498a9357046b5f
Ben <bencaine1@gmail.com>
2018-05-22 21:31:34 -0400

    Added date pickers for start and end dates

--- app/src/main/java/com/teamawesome/fellowship/CreateInitiativeActivity.java
+++ app/src/main/java/com/teamawesome/fellowship/CreateInitiativeActivity.java
@@ -1,13 +1,38 @@
 package com.teamawesome.fellowship;
 
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CreateInitiativeActivity extends AppCompatActivity {
+
+    @BindView(R.id.startDate) EditText _startDate;
+    @BindView(R.id.startDate) EditText _endDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_initiative);
+        ButterKnife.bind(this);
+    }
+
+    public void showStartDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment(_startDate);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showEndDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment(_endDate);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
---
+++ app/src/main/java/com/teamawesome/fellowship/DatePickerFragment.java
@@ -0,0 +1,52 @@
+package com.teamawesome.fellowship;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v4.app.DialogFragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    private Calendar c;
+    private EditText dateField;
+
+    public DatePickerFragment() {}
+
+    public DatePickerFragment(EditText dateField) {
+        this.dateField = dateField;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, day);
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        //EditText startDate = ((CreateInitiativeActivity) getActivity()).getStartDateField();
+        dateField.setText(sdf.format(c.getTime()));
+    }
+
+}
